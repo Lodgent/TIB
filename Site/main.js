3e73c1ca63cc7bdcd0e9c6aeeb884f71ca526f49
Serkik <63666583+Serckik@users.noreply.github.com>
2022-10-21 21:34:14 +0500

    add level 1 map on site and test inventory

--- Site/main.js
+++ Site/main.js
@@ -1,44 +1,51 @@
 const requestURL = 'http://26.100.4.13:5000'
 let level = 1
-let levelH = 9
-let levelW = 15
-let div = document.createElement('div')
+let levelH = 10
+let levelW = 16
+
 let firstUl = document.createElement('ul')
 firstUl.className = "game"
 for (let i = 0; i <levelW; i++) {
     let ull = document.createElement('ul')
     for (let j = 0; j < levelH; j++) {
         let li = document.createElement('li');
-        li.onclick = function() { POSTRequest(i, j) }
+        li.onclick = function() { POSTRequest(j, 0, i) }
         ull.append(li)
     }
     firstUl.append(ull)
 }
-div.append(firstUl)
+document.body.children[0].append(firstUl)
 
-document.body.append(div)
+let invul = document.createElement('ul')
+document.body.children[1].append(invul)
+
 
 setInterval(function GETRequest() {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
+        /*let invli = document.createElement('li')
+        let invtext = document.createElement('p')
+        invtext.textContent = "im button"
+        invli.append(invtext)
+        invul.append(invli)*/
 
         xhr.open('POST', 'http://26.100.4.13:5000/get_action?device=SITE');
 
         xhr.onload = () => {
             var a = JSON.parse(xhr.response)
-            if(JSON.stringify(a) != '{"action":"Something went wrong"}'){
-              var audio = new Audio('VineBoom.mp3');
-              audio.loop = false;
-              audio.play()
+            if(JSON.stringify(a) == '{"action":"GiveButton"}'){
+                let text = document.createElement('p')
+                text.textContent = "im button"
+                document.body.children[1].append(text)
             }
         }
         xhr.send()
     })}, 1000)
 
-function POSTRequest(i, j) {
+function POSTRequest(x, y, z) {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
 
-        xhr.open('POST', 'http://26.100.4.13:5000/action?action='+i+' '+j+'&device=VR');
+        xhr.open('POST', 'http://26.100.4.13:5000/action?action=SpawnEscapeButton:'+x+','+y+','+z+'&device=VR');
         xhr.send()
     })}
