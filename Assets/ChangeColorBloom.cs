414ea9cc2683a4b6fa22c678ceeec791fa1da4a1
Ramil Mavlyutov <stirk2281337@gmail.com>
2022-11-05 19:12:59 +0500

    Fix water, fix shadows, add souns and lights to platform

---
+++ Assets/ChangeColorBloom.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorBloom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gameObject;
+    public AudioSource activated;
+    private bool IsTriggered;
+    public Light light;
+    void Start()
+    {
+        IsTriggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeColor()
+    {
+        if (!IsTriggered)
+        {
+            Material mymat = gameObject.GetComponent<Renderer>().material;
+            mymat.color = Color.green;
+            mymat.SetColor("_EmissionColor", Color.green);
+            activated.Play();
+            light.color = Color.green;
+            IsTriggered = true;
+        }
+
+    }
+}
